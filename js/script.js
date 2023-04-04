f876fbbaa75fe23911cb98204eb2cca942dda42e
cvonk02 <cvonk@hawk.iit.edu>
2023-04-04 15:18:11 -0500

    Added javascript code and Updated readme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
  
+    var message = document.getElementById("entryinput");
+	var button = document.getElementById("entrybutton");
 
+	button.addEventListener("click", function () {
+		alert("Carter Vonk: " + message.value);
+		document.getElementById("textoutput").innerHTML = message.value
+	});
 
-
-
-
-
-
+    }
 window.addEventListener('load', init);
